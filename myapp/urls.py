c765a3ca713fa615807f45292ef4d55456832a62
intiqamquluzada <quluzadintiqam@gmail.com>
2023-10-17 20:23:51 +0400

    last

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from myapp.views import index_view
+from myapp.views import index_view, detail_view
 
 urlpatterns = [
     path("", index_view, name="index"),
+    path("detail/<int:id>/", detail_view, name='detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -10,3 +10,11 @@ def index_view(request):
         "my_datas": my_datas,
     }
     return render(request, "index.html", context)
+
+
+def detail_view(request, id):
+    my_obj = MyModel.objects.get(id=id)
+    context = {
+        "my_obj": my_obj,
+    }
+    return render(request, "detail.html", context)
